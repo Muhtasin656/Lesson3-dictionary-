97ca453db8216bab4dbdeeb43fe87e35ae674bbd
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-08-15 16:38:30 +1000

    new

---
+++ Activity3.py
@@ -0,0 +1,5 @@
+country_code={"India":"0091","Australia":"0025","nepal":"00977"}
+print("the country code of Australia is")
+print(country_code.get("Australia","not found"))
+print("the country code of Japan is")
+print(country_code.get("Japan","not found"))
